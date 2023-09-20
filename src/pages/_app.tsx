9f8615ad49f46a1c7eb718df8b90fda2937bd0e0
muhammadgata <iamgata.me@gmail.com>
2023-09-20 18:14:53 +0700

    feat(font): setup font with next/font/google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+  variable: "--font-space-grotesk",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-space-grotesk: ${spaceGrotesk.style.fontFamily};
+        }
+        html {
+          font-family: var(--font-space-grotesk);
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
